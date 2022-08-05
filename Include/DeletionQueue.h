99f33c54cdc2bc27eaea0b5688a07fef4e1a49e6
dogukannn <dogukan.ozturk@mediaz.io>
2022-08-05 15:53:12 +0300

    vulkan deletion queue v2

--- Include/DeletionQueue.h
+++ Include/DeletionQueue.h
@@ -13,16 +13,17 @@ namespace mz::vk
 	{
 		DeletionQueue(Device* Vk);
 		
-		void AddFenceForDeletor(std::function<void()>&& deletor, VkFence m_Fence);
+		
+		void EnqueueDeletion(std::function<void()>&& function, std::vector<VkFence> m_Fences);
 		void EnqueueDeletion(std::function<void()>&& function, VkFence m_Fence);
 		void Flush();
 		
 		void Run() override;
 
 		private:
-			mz::ThreadSafeQueue<std::function<void()>> m_Deletors;
-			//std::deque<std::function<void()>> m_Deletors;
-			std::map<std::function<void()>, std::vector<VkFence>> m_FencesToWait;
+			mz::ThreadSafeQueue < std::pair<std::function<void()>, std::vector<VkFence>> > m_Deletors;
+
+			
 	};
 
 
--- Source/DeletionQueue.cpp
+++ Source/DeletionQueue.cpp
@@ -14,51 +14,29 @@ namespace mz::vk
 	{
 	}
 
-	void DeletionQueue::AddFenceForDeletor(std::function<void()>&& deletor, VkFence m_Fence)
-	{
-		if (!m_Fence)
-		{
-			return;
-		}
-		if (m_FencesToWait.contains(deletor))
-		{
-			m_FencesToWait[deletor].push_back(m_Fence);
-		}
-		std::vector<VkFence> fences;
-		fences.push_back(m_Fence);
-		m_FencesToWait.insert({ deletor, fences });
-	}
-
 	void DeletionQueue::EnqueueDeletion(std::function<void()>&& deletor, VkFence m_Fence)
 	{
-		m_Deletors.push_back(deletor);
-		if (!m_Fence)
-		{
-			return;
-		}
-		if (m_FencesToWait.contains(deletor))
-		{
-			m_FencesToWait[deletor].push_back(m_Fence);
-		}
 		std::vector<VkFence> fences;
 		fences.push_back(m_Fence);
-		m_FencesToWait.insert({ deletor, fences });
+		m_Deletors.push_back({ deletor, fences });
+	}
+
+	void DeletionQueue::EnqueueDeletion(std::function<void()>&& deletor, std::vector<VkFence> m_Fences)
+	{
+		m_Deletors.push_back({ deletor, m_Fences });
 	}
 
 	void DeletionQueue::Flush()
 	{
-		std::function<void()> deletor;
-		while (m_Deletors.try_pop_front(&deletor))
+		std::pair<std::function<void()>, std::vector<VkFence>> pair;
+		while (m_Deletors.try_pop_front(&pair))
 		{
-			if (m_FencesToWait.contains(deletor))
+			auto fences = pair.second;
+			while (Vk->WaitForFences(fences.size(), fences.data(), VK_TRUE, 10000) != VK_SUCCESS)
 			{
-				auto fences = m_FencesToWait[deletor];
-				while (Vk->WaitForFences(fences.size(), fences.data(), VK_TRUE, 10000) != VK_SUCCESS)
-				{
 
-				}
 			}
-			deletor();
+			pair.first();
 		}
 	}
 
