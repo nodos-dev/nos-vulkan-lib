f8a9dc880cc7c4121affafe4c7ce8c528bab761c
atkurtul <atil.kurtulmus@zerodensity.tv>
2022-12-06 15:46:12 +0300

    mask checkpoint

--- Source/Device.cpp
+++ Source/Device.cpp
@@ -13,11 +13,11 @@ static std::vector<const char*> layers = {
 //       we can accept a parameter to the engine to enable the validation layer.
 //       This can help us in times of need even in production builds.
 //
-// #ifdef MZ_DEV_BUILD
-// #pragma message("Development build: Enabling VK_LAYER_KHRONOS_validation, VK_LAYER_KHRONOS_synchronization2")
-//     "VK_LAYER_KHRONOS_validation",
-//      "VK_LAYER_KHRONOS_synchronization2",
-// #endif
+ #ifdef MZ_DEV_BUILD
+ #pragma message("Development build: Enabling VK_LAYER_KHRONOS_validation, VK_LAYER_KHRONOS_synchronization2")
+     "VK_LAYER_KHRONOS_validation",
+      "VK_LAYER_KHRONOS_synchronization2",
+ #endif
 };
 
 static std::vector<const char*> extensions = {
