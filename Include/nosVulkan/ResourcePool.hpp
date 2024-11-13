4f5671472480c4dc6c56244be7729c0f6374099f
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-11-13 12:07:03 +0300

    From 1.2: Minimize criticial section when creating resources

--- Include/nosVulkan/ResourcePool.hpp
+++ Include/nosVulkan/ResourcePool.hpp
@@ -43,9 +43,11 @@ public:
 		auto freeIt = Free.find(info);
 		if (freeIt == Free.end() || freeIt->second.empty())
 		{
+			guard.unlock();
 			auto res = ResourceT::New(Device, info); //was typename
 			if (!res)
 				return nullptr;
+			guard.lock();
 			Used[uint64_t(res->Handle)] = { tag, info, res };
 			UsedResourceMemoryUsage += res->Size;
 			CheckAndClean();
